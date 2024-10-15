bcc1c942c6c476b00ddd2693eeccb283ccbdc154
Medliarskyi <med.uras111@gmail.com>
2024-10-16 00:10:52 +0300

    first master commit

---
+++ UnitTest62it/UnitTest62it.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab62it/lab62it.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest62it
+{
+	TEST_CLASS(UnitTest62it)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			int arr[] = { 1, 2, 3, 4, 5 };
+			int expected[] = { 5, 4, 3, 2, 1 };
+
+			reverseArray(arr, 5);
+
+			for (int i = 0; i < 5; ++i) {
+				Assert::AreEqual(expected[i], arr[i]);  
+			}
+		}
+	};
+}
---
+++ lab62it/lab62it.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cstdlib> // для rand
+
+using namespace std;
+
+void createArray(int* arr, int n) {
+    for (int i = 0; i < n; ++i) {
+        arr[i] = rand() % 100;
+    }
+}
+
+void printArray(const int* arr, int n) {
+    for (int i = 0; i < n; ++i) {
+        cout << setw(4) << arr[i];
+    }
+    cout << endl;
+}
+
+void reverseArray(int* arr, int n) {
+    for (int i = 0; i < n / 2; ++i) {
+        int temp = arr[i];
+        arr[i] = arr[n - i - 1];
+        arr[n - i - 1] = temp;
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    int* arr = new int[n];
+    createArray(arr, n);
+    cout << "Initial array: ";
+    printArray(arr, n);
+
+    reverseArray(arr, n);
+    cout << "The array is in reverse order: ";
+    printArray(arr, n);
+
+    delete[] arr;
+    return 0;
+}
